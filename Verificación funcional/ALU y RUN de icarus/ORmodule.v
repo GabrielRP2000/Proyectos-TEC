1930216926b956739a8261d38651462dbf676cf8
GabrielRP2000 <111942339+GabrielRP2000@users.noreply.github.com>
2025-02-12 11:26:29 -0600

    Delete Verificación funcional/ALU y RUN de icarus directory

--- Verificación funcional/ALU y RUN de icarus/ORmodule.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module ORmodule(A,B,R);
-	input [15:0] A,B;
-	output [15:0] R;
-
-	assign R=A|B;
-	
-endmodule
--- Verificación funcional/ALU y RUN de icarus/XORmodule.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-module XORmodule(A,B,R);
-	input [15:0] A,B;
-	output [15:0] R;
-	
-	assign R=A^B;
-endmodule
